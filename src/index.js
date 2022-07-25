fe83720a028875b1ef5814d689fb52da407cac88
akashguptasky <skygupta193@gmail.com>
2022-07-25 12:02:19 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const router = require('./route/route')
+const router = require('./routes/route')
 
 
 const app  = express();
